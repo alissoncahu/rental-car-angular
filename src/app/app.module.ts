a3c3630a6639d4347dba583f39940a7eaa982593
Alisson Barbosa <alisson.barbosa@mv.com.br>
2024-03-03 13:36:24 -0300

    Criação das views

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './view/users/users.component';
+import { CardComponent } from './view/card/card.component';
+import { LocationsComponent } from './view/locations/locations.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    CardComponent,
+    LocationsComponent
   ],
   imports: [
     BrowserModule,
